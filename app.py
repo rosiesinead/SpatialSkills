8e6d2db6e309f24bb7745f8fb2625b88c8e8e035
rosiesinead <2225814t@student.gla.ac.uk>
2019-07-30 15:59:11 +0100

    updated relative paths on admin pages

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ class Exercises(db.Model):
   
 @app.route('/')
 def spatialskills():
-    getEx1 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.section==1).all()
+    getEx1 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.exercise_number==1).all()
     exercise1 = [x[0] for x in getEx1]
-    getEx2 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.section==2).all()
+    getEx2 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.exercise_number==2).all()
     exercise2 = [x[0] for x in getEx2]
-    getEx3 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.section==3).all()
+    getEx3 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.exercise_number==3).all()
     exercise3 = [x[0] for x in getEx3]
-    getEx4 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.section==4).all()
+    getEx4 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.exercise_number==4).all()
     exercise4 = [x[0] for x in getEx4]
-    getEx5 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.section==5).all()
+    getEx5 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.exercise_number==5).all()
     exercise5 = [x[0] for x in getEx5]
-    getEx6 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.section==6).all()
+    getEx6 = Exercises.query.with_entities(Exercises.exercise_data).filter(Exercises.exercise_number==6).all()
     exercise6 = [x[0] for x in getEx6]
     return render_template("spatialskills/index.html", exercise1=exercise1,exercise2=exercise2,exercise3=exercise3,exercise4=exercise4,exercise5=exercise5,exercise6=exercise6)
 
